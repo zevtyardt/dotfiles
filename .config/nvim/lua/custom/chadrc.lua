274b87f3733fdbf6421c680ad3f1a960a7103055
zevtyardt <apklover76@gmail.com>
2023-05-18 21:58:13 +0700

    ...

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,5 +1,8 @@
 ---@type ChadrcConfig 
  local M = {}
- M.ui = {theme = 'catppuccin'}
+ M.ui = {theme = 'nightfox'}
  M.plugins = 'custom.plugins'
  return M
+
+
+  
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,9 +1,33 @@
+local opts = {}
+vim.api.nvim_set_keymap("v", "<C-r>", "<CMD>SearchReplaceSingleBufferVisualSelection<CR>", opts)
+vim.api.nvim_set_keymap("v", "<C-s>", "<CMD>SearchReplaceWithinVisualSelection<CR>", opts)
+vim.api.nvim_set_keymap("v", "<C-b>", "<CMD>SearchReplaceWithinVisualSelectionCWord<CR>", opts)
+
+vim.api.nvim_set_keymap("n", "<leader>rs", "<CMD>SearchReplaceSingleBufferSelections<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>ro", "<CMD>SearchReplaceSingleBufferOpen<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>rw", "<CMD>SearchReplaceSingleBufferCWord<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>rW", "<CMD>SearchReplaceSingleBufferCWORD<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>re", "<CMD>SearchReplaceSingleBufferCExpr<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>rf", "<CMD>SearchReplaceSingleBufferCFile<CR>", opts)
+
+vim.api.nvim_set_keymap("n", "<leader>rbs", "<CMD>SearchReplaceMultiBufferSelections<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>rbo", "<CMD>SearchReplaceMultiBufferOpen<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>rbw", "<CMD>SearchReplaceMultiBufferCWord<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>rbW", "<CMD>SearchReplaceMultiBufferCWORD<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>rbe", "<CMD>SearchReplaceMultiBufferCExpr<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>rbf", "<CMD>SearchReplaceMultiBufferCFile<CR>", opts)
+
+-- show the effects of a search / replace in a live preview window
+vim.o.inccommand = "split"
+
+
 local plugins = {
   {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "rust-analyzer"
+        "rust-analyzer",
+        "jedi-language-server"
       }
     }
   },
@@ -32,5 +56,61 @@ local plugins = {
       require("rust-tools").setup(opts)
     end
   },
+  {
+    "farmergreg/vim-lastplace",
+    lazy = false,
+    init = function()
+      vim.g.lastplace_ignore_buftype = "quickfix,nofile,help"
+      vim.g.lastplace_ignore_filetype = "gitcommit,gitrebase,svn,hgcommit"
+      vim.g.lastplace_open_folds = 1
+    end
+  },
+
+  {
+    "roobert/search-replace.nvim",
+    lazy = false,
+    config = function()
+      require("search-replace").setup({
+        -- optionally override defaults
+        default_replace_single_buffer_options = "gcI",
+        default_replace_multi_buffer_options = "egcI",
+      })
+    end,
+    opts = opts,
+  },
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+    config = function()
+      require("auto-session").setup {
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+      }
+    end
   }
+
+   --[[{
+    "krivahtoo/silicon.nvim",
+    lazy = false,
+    build = "./install.sh build",
+    cmd = {"Silicon"},
+    config = function()
+      require('silicon').setup {
+        line_number = true,
+        pad_vert = 80,
+        pad_horiz = 50,
+        output = {
+          path = "/sdcard/Pictures/"
+        },
+        watermark = {
+          text = '  @zevtyardt',
+        },
+        window_title = function()
+          return vim.fn.fnamemodify(vim.fn.bufname(vim.fn.bufnr()), ':~:.')
+        end,
+      }
+    end
+  }--]]
+}
 return plugins
+
